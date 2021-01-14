dad2e42ed4c659de72061cdee5a18a74b8da0790
Marc Kenneth Lomio <drmkcthehandsome@gmail.com>
2021-01-14 10:53:35 +0800

    create users with bcrypt password

--- go-rest-api-practice-with-jwt-main.go
+++ go-rest-api-practice-with-jwt-main.go
@@ -6,9 +6,10 @@ import(
 "log"
 "net/http"
 "github.com/gorilla/mux"
- "io/ioutil"
+"io/ioutil"
 "gorm.io/gorm"
 "gorm.io/driver/sqlite"
+"golang.org/x/crypto/bcrypt"
 )
 // Global Variables
 type Product struct {
@@ -17,7 +18,13 @@ type Product struct {
  Price uint
 }
 
-//TO DO: Refactor follow DRY principle
+type User struct {
+ gorm.Model
+ Email string    `json:"email" gorm:"unique"` 
+ Password string `json:"password"`
+}
+
+//TO DO: Refactor follow DRY principle & Delegation Principle
 
 // Trigger Functions at start  
 func homePage(w http.ResponseWriter, r *http.Request){
@@ -43,7 +50,8 @@ func createDatabaseSchema(){
  
     // Migrate the schema
     db.AutoMigrate(&Product{})
- 
+    db.AutoMigrate(&User{})
+
     // Create
     db.Create(&Product{Code: "P1", Price: 100})
 }
@@ -60,6 +68,8 @@ func initRoutesByGorillaMux(){
    myRouter.HandleFunc("/products", returnAllProducts).Methods("GET")
    myRouter.HandleFunc("/product/{id}", deleteProduct).Methods("DELETE")
    myRouter.HandleFunc("/product/{code}",returnSingleProduct).Methods("GET")
+   myRouter.HandleFunc("/user", createNewUser).Methods("POST")
+   myRouter.HandleFunc("/users", returnAllUsers).Methods("GET")
    log.Fatal(http.ListenAndServe(":9000", myRouter))
 }
 
@@ -140,4 +150,47 @@ func updateProduct(w http.ResponseWriter, r *http.Request){
     db.Model(&product).Where("id = ?", key).Updates(Product{Code: product.Code, Price: product.Price})
     json.NewEncoder(w).Encode(product)
 }
+
+func createNewUser(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: createNewUser")
+	db, err := gorm.Open(sqlite.Open("practice.db"), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+    reqBody, _ := ioutil.ReadAll(r.Body)
+    var user User 
+	var hash_password string = ""
+	hash_password = hashPassword(user.Password)
+    json.Unmarshal(reqBody, &user)
+    db.Create(&User{Email: user.Email, Password: hash_password})
+	user.Password = hash_password
+    json.NewEncoder(w).Encode(user)
+}
+
+func hashPassword(password string) string {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "failed generate bcrypt password"
+	}
+    
+	var hash_password string = ""
+	hash_password = string(bytes)
+
+	return hash_password
+}
+
+func returnAllUsers(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllUsers")
+    db, err := gorm.Open(sqlite.Open("practice.db"), &gorm.Config{})
+    
+    if err != nil {
+        panic("failed to connect database")
+    }
+	
+    var users []User
+    db.Find(&users)
+    json.NewEncoder(w).Encode(users)
+}
+
+
 
