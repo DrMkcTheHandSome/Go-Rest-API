08e66f4c9235e7ba554733ac504552ee163af8e4
Marc Kenneth Lomio <drmkcthehandsome@gmail.com>
2021-01-14 14:14:36 +0800

    jwt & cookies setup

--- go-rest-api-practice-with-jwt-main.go
+++ go-rest-api-practice-with-jwt-main.go
@@ -10,6 +10,8 @@ import(
 "gorm.io/gorm"
 "gorm.io/driver/sqlite"
 "golang.org/x/crypto/bcrypt"
+jwt "github.com/dgrijalva/jwt-go"
+"time"
 )
 // Global Variables
 type Product struct {
@@ -22,6 +24,19 @@ type User struct {
  gorm.Model
  Email string    `json:"email" gorm:"unique"` 
  Password string `json:"password"`
+}
+
+// JwtWrapper wraps the signing key and the issuer
+type JwtWrapper struct {
+	SecretKey       string
+	Issuer          string
+	ExpirationHours int64
+}
+
+// JwtClaim adds email as a claim to the token
+type JwtClaim struct {
+	Email string
+	jwt.StandardClaims
 }
 
 //TO DO: Refactor follow DRY principle & Delegation Principle
@@ -53,7 +68,7 @@ func createDatabaseSchema(){
     db.AutoMigrate(&User{})
 
     // Create
-    db.Create(&Product{Code: "P1", Price: 100})
+    //db.Create(&Product{Code: "P1", Price: 100}) // test
 }
 
 func initializeRoutes(){
@@ -69,6 +84,7 @@ func initRoutesByGorillaMux(){
    myRouter.HandleFunc("/product/{id}", deleteProduct).Methods("DELETE")
    myRouter.HandleFunc("/product/{code}",returnSingleProduct).Methods("GET")
    myRouter.HandleFunc("/user", createNewUser).Methods("POST")
+   myRouter.HandleFunc("/user/login", loginUser).Methods("POST")
    myRouter.HandleFunc("/users", returnAllUsers).Methods("GET")
    log.Fatal(http.ListenAndServe(":9000", myRouter))
 }
@@ -160,23 +176,11 @@ func createNewUser(w http.ResponseWriter, r *http.Request){
     reqBody, _ := ioutil.ReadAll(r.Body)
     var user User 
 	var hash_password string = ""
-	hash_password = hashPassword(user.Password)
     json.Unmarshal(reqBody, &user)
+    hash_password = hashPassword(user.Password)
     db.Create(&User{Email: user.Email, Password: hash_password})
 	user.Password = hash_password
     json.NewEncoder(w).Encode(user)
-}
-
-func hashPassword(password string) string {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	if err != nil {
-		return "failed generate bcrypt password"
-	}
-    
-	var hash_password string = ""
-	hash_password = string(bytes)
-
-	return hash_password
 }
 
 func returnAllUsers(w http.ResponseWriter, r *http.Request){
@@ -191,6 +195,92 @@ func returnAllUsers(w http.ResponseWriter, r *http.Request){
     db.Find(&users)
     json.NewEncoder(w).Encode(users)
 }
+
+func loginUser(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: loginUser")
+	  db, err := gorm.Open(sqlite.Open("practice.db"), &gorm.Config{})
+    
+    if err != nil {
+        panic("failed to connect database")
+    }
+	
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var user User 
+	var userPayload User
+	json.Unmarshal(reqBody, &user)
+	userPayload = user
+    query := db.Where(&User{Email: user.Email}).First(&user)
+	if query.Error == gorm.ErrRecordNotFound {
+	    fmt.Println("Login Failed")
+	} 
+	 err = checkPassword(user.Password,userPayload.Password)
+	 if err != nil {
+	    fmt.Println("Login Failed")
+	 } else {
+	    fmt.Println("Login Success")
+        initJWT(w,r,user)
+	 }
+}
+
+func initJWT(w http.ResponseWriter, r *http.Request,user User){
+		jwtWrapper :=  &JwtWrapper {
+		SecretKey:       "verysecretkey",
+		Issuer:          "AuthService",
+		ExpirationHours: 24,
+	}
+	
+  // a token that expires in 24 hours
+  	expirationTime := time.Now().Add(1440 * time.Minute)
+	
+	claims := &JwtClaim{
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			Issuer:    jwtWrapper.Issuer,
+		},
+	}
+	
+	// generates token 
+   token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+   // Create JWT string
+   tokenString, err := token.SignedString([]byte(jwtWrapper.SecretKey))
+    if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+	
+	json.NewEncoder(w).Encode(tokenString)
+}
+
+func hashPassword(password string) string {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "failed generate bcrypt password"
+	}
+    
+	var hash_password string = ""
+	hash_password = string(bytes)
+
+	return hash_password
+}
+
+  func checkPassword(userPasswordfromDB,providedPassword string) error {
+	  err := bcrypt.CompareHashAndPassword([]byte(userPasswordfromDB), []byte(providedPassword))
+	  if err != nil {
+		  return err
+	  }
+
+	  return nil
+ }
+
+
 
 
 
