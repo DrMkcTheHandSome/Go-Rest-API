9b4cab642cc0dc04aa7b5d8eae68c5b25577840a
Marc Kenneth Lomio <drmkcthehandsome@gmail.com>
2021-01-21 11:42:46 +0800

    refactor repo connection

--- data/repositories/repositories.go
+++ data/repositories/repositories.go
@@ -14,11 +14,7 @@ import(
 	func SchemaMigration(){
 		fmt.Println("services SchemaMigration")
 
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-        if err != nil {
-            panic("failed to connect database")
-        }
-     
+		db := InitDB()		   
         //Migrate the schema
         db.Migrator().CreateTable(&entities.Product{})
         db.Migrator().CreateTable(&entities.User{})	
@@ -26,32 +22,22 @@ import(
 
 	func GetAllProducts() []entities.Product  {
 		fmt.Println("repository GetAllProducts")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
-		   
-	var products []entities.Product
+		db := InitDB()		   
+	 var products []entities.Product
 	db.Raw("select * from products").Scan(&products)
       return products
 	}
 
 	func CreateNewProduct(product entities.Product) entities.Product {
 		fmt.Println("repository CreateNewProduct")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
+		db := InitDB()
 	   db.Exec("INSERT INTO products (created_at,code,price) VALUES (?,?,?)",time.Now(), product.Code,product.Price)
 	   return product
 	}
 
 	func GetSingleProduct(productId string) entities.Product {
 		fmt.Println("repository GetSingleProduct")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
+		db := InitDB()
 		 var product entities.Product
 		 db.Raw("select * from products where id = ?",productId).Scan(&product)  
 		 return product
@@ -59,30 +45,20 @@ import(
 
 	func UpdateProduct(productId string, product entities.Product) {
 		fmt.Println("repository UpdateProduct")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
+		 db := InitDB()
 		 db.Exec("UPDATE products SET code=?, price = ? WHERE id = ?", product.Code, product.Price, productId)
 	}
 
 	func DeleteProduct(productId string){
 		fmt.Println("repository DeleteProduct")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
-
+		db := InitDB()
 		db.Exec("DELETE FROM products WHERE id = ?", productId)
 	}
 
 	func CreateNewUser(user entities.User, hash_password string,is_email_verified bool) entities.User {
 		fmt.Println("repository CreateNewUser")
 
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
+		db := InitDB()
 
 	    db.Exec("INSERT INTO users (created_at,email,password,is_email_verified) VALUES (?,?,?,?)",time.Now(), user.Email,hash_password,is_email_verified)
         return user
@@ -91,23 +67,25 @@ import(
 	
 	func GetAllUsers() []entities.User  {
 		fmt.Println("repository GetAllUsers")
-		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
-		if err != nil {
-           panic("failed to connect database")
-		   }
-
-	var users []entities.User
-	db.Raw("select * from users").Scan(&users)
-      return users
+		db := InitDB()
+	    var users []entities.User
+	    db.Raw("select * from users").Scan(&users)
+       return users
 	}
 
 	func GetUserByEmail(email string) entities.User {
 		fmt.Println("repository GetUserByEmail")
+		 db := InitDB()
+		 var user entities.User
+		 db.Raw("select * from users where email = ?",email).Scan(&user)  
+		 return user
+	}
+
+	func InitDB() *gorm.DB {
 		db, err := gorm.Open(sqlserver.Open(connections.ConnectionString), &gorm.Config{})
 		if err != nil {
            panic("failed to connect database")
 		   }
-		 var user entities.User
-		 db.Raw("select * from users where email = ?",email).Scan(&user)  
-		 return user
+
+		return db
 	}
--- go.mod
+++ go.mod
@@ -8,12 +8,17 @@ require (
 	controllers v1.2.3
 	docs v1.2.3
 	entities v1.2.3
+	github.com/codegangsta/negroni v1.0.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/pjebs/jsonerror v0.0.0-20190614034432-63ef9a8df848 // indirect
+	github.com/pjebs/restgate v0.0.0-20200504001537-fd9a58a4fe75 // indirect
 	github.com/swaggo/http-swagger v1.0.0
 	globalvariables v1.2.3
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3
+	gopkg.in/unrolled/render.v1 v1.0.0 // indirect
 	gorm.io/driver/sqlserver v1.0.5
 	gorm.io/gorm v1.20.11
 	helpers v1.2.3
